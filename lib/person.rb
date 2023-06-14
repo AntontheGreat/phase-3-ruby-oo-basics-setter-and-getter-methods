51b75eb76a30be21345b0813c93368e206af1a61
Antonio <antonio18casta@aol.com>
2023-06-14 16:44:30 -0400

    commit tree c9deea9974ac98d058150bd83a6663f4ed1ba3a3

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+    # Define a name getter and a name= setter method for the instance variable @name.
+    # Define a job getter and a job= setter method for the instance variable @job.
 
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
 end
